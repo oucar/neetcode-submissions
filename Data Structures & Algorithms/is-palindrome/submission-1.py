6b117d93f709a6f901a035b2c690794d4d489eec
Onur Ucar <hello@ucaronur.com>
2026-08-14 16:30:27 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        # remove alphanumeric 
+        s = "".join(c.lower() for c in s if c.isalnum())
+
+        left, right = 0, len(s) - 1
+
+        while left < right: 
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
