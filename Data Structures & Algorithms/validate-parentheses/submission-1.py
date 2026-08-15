a4f27a46cb2dccb41668ba894921e69b0a23ce1f
Onur Ucar <hello@ucaronur.com>
2026-08-15 08:51:12 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+        closing_to_opening = {
+            ']' : '[',
+            ')' : '(',
+            '}' : '{',
+        }
+
+
+        for char in s:
+            is_closing_bracket = char in closing_to_opening 
+
+            if is_closing_bracket:
+                if len(stack) == 0:
+                    return False
+            
+                # expected will be an opening bracket 
+                expected = closing_to_opening[char]
+                # look at the most recent unmatched
+                # bracket
+                most_recent = stack[-1]
+
+                if most_recent != expected:
+                    return False
+                
+                stack.pop()
+
+            else: 
+                stack.append(char)
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
